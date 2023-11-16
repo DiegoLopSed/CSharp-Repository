72f4b49c98acc2f9bcda6919ab2038440531bbd6
DiegoHubLopez <diegolopsed160703@gmail.com>
2023-11-15 22:54:47 -0600

    Estructura base de un programa C#

---
+++ SintaxisC#/BaseC#.cs
@@ -0,0 +1,18 @@
+using System; //! No remover
+
+namespace MiPrimerProyecto //! Debe ser remplazado por el nombre de tu proyecto
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");//* Mostrar a pantalla el mensaje
+			
+			// TODO: Funcionalidad del codigo dentro de las llaves Main
+			
+			Console.Write("Press any key to continue . . . "); 
+			Console.ReadKey(true);
+
+		} //? Fin llave Main
+	} //? Fin llave Class
+}//? Fin llave namespace
