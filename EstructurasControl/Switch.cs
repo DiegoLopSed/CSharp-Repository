8cbb846a533ed045edf6a5490126b369dada98a8
DiegoHubLopez <diegolopsed160703@gmail.com>
2023-11-19 01:06:24 -0600

    Estructura de Control Switch en C#

--- EstructurasControl/Switch.cs
+++ EstructurasControl/Switch.cs
@@ -4,6 +4,9 @@ class Program
 {
     static void Main()
     {
+        //! Estructura de control Switch
+
+
         //? Mensajes para que el usuario seleccione una opción
         Console.WriteLine("Selecciona una opción:");
         Console.WriteLine("1. Saludo");
