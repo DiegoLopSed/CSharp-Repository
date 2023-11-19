a11fb1b1d908a14b8123f389b9ec345c6fc10afc
DiegoHubLopez <diegolopsed160703@gmail.com>
2023-11-19 01:05:45 -0600

    Estructura de Control ElseIf en C#

--- EstructurasControl/Elseif.cs
+++ EstructurasControl/Elseif.cs
@@ -2,40 +2,43 @@ using System;
 
 namespace MiPrimerProyecto
 {
-	class Program
-	{
-		public static void Main(string[] args)
-		{
-		 //! Solicitar al usuario que ingrese una calificación
-        Console.Write("Ingresa tu calificación: ");
-        int calificacion = Convert.ToInt32(Console.ReadLine());
+    class Program
+    {
+        public static void Main(string[] args)
+        {
 
-        //? Estructura condicional para evaluar la calificación
-        if (calificacion >= 90)
-        {
-            //* Si la calificación es igual o mayor a 90, se imprime "¡Excelente!"
-            Console.WriteLine("¡Excelente!");
+            //! Estructura de control Else If
+
+            //! Solicitar al usuario que ingrese una calificación
+            Console.Write("Ingresa tu calificación: ");
+            int calificacion = Convert.ToInt32(Console.ReadLine());
+
+            //? Estructura condicional para evaluar la calificación
+            if (calificacion >= 90)
+            {
+                //* Si la calificación es igual o mayor a 90, se imprime "¡Excelente!"
+                Console.WriteLine("¡Excelente!");
+            }
+            else if (calificacion >= 70)
+            {
+                //* Si la calificación está entre 70 y 89, se imprime "Buen trabajo"
+                Console.WriteLine("Buen trabajo");
+            }
+            else if (calificacion >= 50)
+            {
+                //* Si la calificación está entre 50 y 69, se imprime "Aprobado"
+                Console.WriteLine("Aprobado");
+            }
+            else
+            {
+                //* Si la calificación es menor a 50, se imprime "Reprobado"
+                Console.WriteLine("Reprobado");
+            }
+            //? Esperar a que el usuario presione una tecla antes de cerrar la aplicación
+
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+
         }
-        else if (calificacion >= 70)
-        {
-            //* Si la calificación está entre 70 y 89, se imprime "Buen trabajo"
-            Console.WriteLine("Buen trabajo");
-        }
-        else if (calificacion >= 50)
-        {
-            //* Si la calificación está entre 50 y 69, se imprime "Aprobado"
-            Console.WriteLine("Aprobado");
-        }
-        else
-        {
-            //* Si la calificación es menor a 50, se imprime "Reprobado"
-            Console.WriteLine("Reprobado");
-        }
-        //? Esperar a que el usuario presione una tecla antes de cerrar la aplicación
-			
-			Console.Write("Press any key to continue . . . ");
-			Console.ReadKey(true);
-        
-		}
-	}
+    }
 }
