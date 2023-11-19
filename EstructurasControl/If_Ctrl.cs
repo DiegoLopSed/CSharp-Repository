01e421f2cbc034204b7190be9059813e178c3b64
DiegoHubLopez <diegolopsed160703@gmail.com>
2023-11-19 01:04:57 -0600

    Estructura de Control If en C#

---
+++ EstructurasControl/If_Ctrl.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace MiPrimerProyecto
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            //! Estructura de control If
+
+            int Edad; //? Declaramos una variable para almacenar la edad
+            string Nombre; //? Declaramos una variable para almacenar el nombre
+
+            Console.Write("Ingresa tu nombre: ");
+            Nombre = Console.ReadLine();//* Leemos el nombre ingresado por el usuario
+            Console.Write("Ingresa tu edad: ");
+            Edad = Convert.ToInt32(Console.ReadLine());//* Leemos la edad ingresada por el usuario
+
+
+            if (Edad >= 18)
+            { //! La estructura if evalua que se cumpla la condición
+
+                //* Acciones por verdadero
+                Console.WriteLine(Nombre + " Eres mayor de edad");
+
+            }
+            else
+            {
+
+                //! Acciones por falso
+                Console.WriteLine(Nombre + " Aún no eres mayor de edad");
+
+            }
+
+
+            //? Esperar a que el usuario presione una tecla antes de cerrar la aplicación
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ EstructurasControl/Switch.cs
@@ -0,0 +1,44 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        //? Mensajes para que el usuario seleccione una opción
+        Console.WriteLine("Selecciona una opción:");
+        Console.WriteLine("1. Saludo");
+        Console.WriteLine("2. Despedida");
+        Console.WriteLine("3. Otro mensaje");
+
+        //! Leer la opción ingresada por el usuario
+        int opcion = Convert.ToInt32(Console.ReadLine());
+
+        //* Utilizar la declaración switch para realizar acciones según la opción seleccionada
+        switch (opcion)
+        {
+            case 1:
+                //* Si la opción es 1, imprimir un saludo
+                Console.WriteLine("Hola, ¡bienvenido!");
+                break;
+
+            case 2:
+                //* Si la opción es 2, imprimir un mensaje de despedida
+                Console.WriteLine("Hasta luego, ¡que tengas un buen día!");
+                break;
+
+            case 3:
+                //* Si la opción es 3, imprimir otro mensaje
+                Console.WriteLine("Este es otro mensaje.");
+                break;
+
+            default:
+                //* Si la opción no es 1, 2 ni 3, imprimir un mensaje de opción no válida
+                Console.WriteLine("Opción no válida. Por favor, selecciona una opción válida.");
+                break;
+        }
+
+        //? Esperar a que el usuario presione una tecla antes de cerrar la aplicación
+        Console.Write("Press any key to continue . . . ");
+        Console.ReadKey(true);
+    }
+}
